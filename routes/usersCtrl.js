fe1bab4d1fe6614ce5d932379a46cb819a49f850
Karl <krmouat22@gmail.com>
2018-04-11 19:59:18 +0200

    added waterfall code blocks from async module to register and login routes

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -2,114 +2,167 @@
 var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils')
 var models = require('../models');
+var asyncLib = require('async')
 
 //Constants
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 //Routes
 module.exports = {
-    register: (req, res) =>{
-       //params front to back mapping
+    register: (req, res) => {
+        //params front to back mapping
 
-       var id_user = req.body.id_user
-       var name_user = req.body.name_user 
-       var firstName_user  = req.body.firstName_user 
-       var password_user = req.body.password_user
-       var email_user = req.body.email_user
+        var id_user = req.body.id_user
+        var name_user = req.body.name_user
+        var firstName_user = req.body.firstName_user
+        var password_user = req.body.password_user
+        var email_user = req.body.email_user
 
-       if ( email_user == null || password_user == null ){
-           return res.status(400).json({
+        if (email_user == null || password_user == null) {
+            return res.status(400).json({
                 'error': 'missing parameters'
-           });
-       }
+            });
+        }
 
-       if (!EMAIL_REGEX.test(email_user)){
-        return res.status(400).json({
-            'error': 'email is not valid'
-          });
-       }
+        if (!EMAIL_REGEX.test(email_user)) {
+            return res.status(400).json({
+                'error': 'email is not valid'
+            });
+        }
 
-       if (!PASSWORD_REGEX.test(password_user)){
-        return res.status(400).json({
-            'error': 'password invalid (must have a length of 4 -8 character and include one number )'
-          });
-       }
+        if (!PASSWORD_REGEX.test(password_user)) {
+            return res.status(400).json({
+                'error': 'password invalid (must have a length of 4 -8 character and include one number )'
+            });
+        }
 
-       
-      // Todo:  verify psuedo length, mail regex, password etc 
-        models.User.findOne({
-            attributes: ['email_user'],
-            where: { email_user: email_user }
-        })
-        .then((userFound) => {
-            if(!userFound) {
-                bcrypt.hash(password_user, 5, (err, bcryptedPassword)=>{
-                    var newUser = models.User.create({
-                        //backend to front-end object mapping
-                        id_user : id_user,
+        asyncLib.waterfall([
+            (done) => {
+                models.User.findOne({
+                        attributes: ['email_user'],
+                        where: {
+                            email_user: email_user
+                        }
+                    })
+                    .then((userFound) => {
+                        done(null, userFound);
+                    })
+                    .catch((err) => {
+                        return res.status(500).json({
+                            'error': 'unable to verify user'
+                        });
+                    });
+            },
+            (userFound, done) => {
+                if (!userFound) {
+                    bcrypt.hash(password_user, 5, (err, bcryptedPassword) => {
+                        done(null, userFound, bcryptedPassword)
+                    });
+                } else {
+                    return res.status(409).json({
+                        'error': 'user already exits'
+                    })
+                }
+            },
+            (userFound, bcryptedPassword, done) => {
+                var newUser = models.User.create({
+                        id_user: id_user,
                         name_user: name_user,
                         firstName_user: firstName_user,
                         password_user: bcryptedPassword,
                         email_user: email_user
                     })
-                    .then((newUser)=>{ 
-                        console.log(this.newUser)
-                        return res.status(201).json({
-                            'id_user': newUser.id_user
+                    .then((newUser) => {
+                        done(newUser)
+                    })
+                    .catch((err) => {
+                        return res.status(500).json({
+                            'error': 'cannot add user'
                         })
                     })
-                    .catch(()=>{
-                        return res.status(500).json({ 'error': 'cannot add user'})
-                    });
+            }
+        ], (newUser) => {
+            if (newUser) {
+                return res.status(201).json({
+                    'id_user': newUser.id_user
                 });
-          } else {
-              return res.status(400).json({'error': 'user already exits'})
-          }
-        })
-        .catch((err)=> {
-            return res.status(500).json({
-                'error': 'unable to verify user'
-            });
-        })
+            } else {
+                return res.status(500).json({
+                    'error': 'cannot add user'
+                });
+            }
+        });
     },
-    login: (req, res)=>{
+    login: (req, res) => {
 
         //Params
-       var  password_user = req.body.password_user;
-       var  email_user = req.body.email_user;
+        var password_user = req.body.password_user;
+        var email_user = req.body.email_user;
 
-       if (password_user == null || email_user == null ){
-        return res.status(400).json({
-             'error': 'missing parameters'
-        });
-       }
-
-     // Todo:  verify psuedo length, mail regex, password etc 
-     models.User.findOne({
-        where: {email_user: email_user}
-     })
-     .then((userFound)=>{
-        if(userFound) {
-
-            bcrypt.compare(password_user, userFound.password_user, (errBcrypt, resBcrypt)=>{
-                if(resBcrypt){
-                    return res.status(200).json({
-                         'id_user': userFound.id_user,
-                         'token': jwtUtils.generateTokenForUser(userFound)
-                    })
-                }else {
-                    return res.status(403).json({'error': 'invlaid password'})
-                }
+        if (password_user == null || email_user == null) {
+            return res.status(400).json({
+                'error': 'missing parameters'
             });
-      } else {
-          return res.status(400).json({'error': 'user does not exist in database'})
-      }
-    })
-    .catch((err)=>{
-        return res.status(500).json({
-            'error': 'unable to verify user'
-        });
-    })
- },
-  
+        }
+
+        if (email_user == null || password_user == null) {
+            return res.status(400).json({
+                'error': 'missing parameters'
+            });
+        }
+
+        asyncLib.waterfall([
+            (done) => {
+                models.User.findOne({
+                        where: {
+                            email_user: email_user
+                        }
+                    })
+                    .then((userFound) => {
+                        done(null, userFound);
+                    })
+                    .catch((err) => {
+                        return res.status(500).json({
+                            'error': 'unable to verify user'
+                        });
+                    });
+            },
+            (userFound, done) => {
+                if (userFound) {
+                    bcrypt.compare(password_user, userFound.password_user, (errBcrypt, resBcrypt) => {
+                        done(null, userFound, resBycrypt);
+                    })
+                } else {
+                    return res.status(404).json({
+                        'error': 'user not exist in DB'
+                    });
+                }
+            },
+            (userFound, resBycrypt, done) => {
+                if (resBycrypt) {
+                    done(userFound);
+                } else {
+                    return res.status(403).json({
+                        'error': 'invalid password'
+                    });
+                }
+            }
+        ], (userFound) => {
+            if (userFound) {
+                return res.status(200).json({
+                    'id_user': userFound.id_user,
+                    'token': jwtUtils.generateTokenForUser(userFound)
+                });
+            } else {
+                return res.status(500).json({
+                    'error': 'cannot log on user'
+                });
+            }
+        })
+    },
+    getUserProfile: () => {
+
+    }
+
+
 }
