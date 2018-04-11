c7447cdfc1aafccd64b6dc93e992b16df26bc354
Thomas Guéquière <thomas.guequiere@gmail.com>
2018-04-11 17:44:25 +0200

    Update 2 : messages

--- routes/messagesCtrl.js
+++ routes/messagesCtrl.js
@@ -1,12 +1,27 @@
 // Imports
 var models = require('../models');
 var asyncLib = require('async');
+var jwtUtils = require('../utils/jwt.utils')
 
 // Constants
 
 // Routes
 module.exports = {
-    func : function(req, es) {
+    createMessage : function(req, es) {
+        // Getting auth headers
+        var headerAuth = req.headers['authorization'];
+        var userId = jwtUtils.getUserId(headerAuth);
+
+        //Params
+        var title = req.body.title;
+        var content = req.body.content;
+
+        if ( title == null || content == null ){
+            return res.status(400).json({'error': 'missing parameters'});
+        }
+    },
+
+    listMessages : function(req, es) {
 
     }
 };
